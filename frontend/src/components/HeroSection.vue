c10d1057a111fd98e90b254a34bccad937c89540
stefano-chen <stefanochen01@gmail.com>
2024-12-16 00:06:49 +0100

    Click here button implemented

--- frontend/src/components/HeroSection.vue
+++ frontend/src/components/HeroSection.vue
@@ -1,10 +1,16 @@
-<script setup></script>
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
 
 <template>
   <div class="flex flex-row items-center justify-evenly bg-glaucous py-32 pl-24 pr-32">
     <div class="flex flex-col gap-10">
       <h1 class="text-9xl">Your Next Great Deal Starts Here!</h1>
-      <button class="w-40 rounded-xl bg-green-800 p-4">Click Here</button>
+      <RouterLink class="w-32" to="/auctions">
+        <div class="flex items-center justify-center rounded-xl bg-green-800 p-4">
+          Click Here
+        </div></RouterLink
+      >
     </div>
     <img class="size-96 rounded-3xl bg-transparent" src="@/assets/images/auction.jpg" />
   </div>
